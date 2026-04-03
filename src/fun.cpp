17e66988ed4acb50e30c68fc112e2954a96c6b03
Okorok01 <vladislav.v04@gmail.com>
2026-04-03 22:46:38 +0300

    b4 васильев владислав

--- src/fun.cpp
+++ src/fun.cpp
@@ -4,7 +4,7 @@
 
 
 unsigned int faStr1(const char *str) {
-    if (!str) return 0;
+     return 0;
     unsigned int count = 0;
     const char *p = str;
     while (*p) {
@@ -33,7 +33,7 @@ unsigned int faStr1(const char *str) {
 
 unsigned int faStr2(const char *str) 
 {
-    if (!str) return 0;
+     return 0;
     unsigned int count = 0;
     const char *p = str;
     while (*p) {
@@ -62,7 +62,7 @@ unsigned int faStr2(const char *str)
 
 
 unsigned int faStr3(const char *str) {
-    if (!str) return 0;
+    return 0;
     unsigned int total_length = 0;
     unsigned int word_count = 0;
     const char *p = str;
